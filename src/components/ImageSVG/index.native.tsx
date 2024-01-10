99d018fa462c1ec2030cd08b7d2471360ae4a88e
Sofonias Abathun <sofoniasabathun@gmail.com>
2024-01-10 03:45:07 +0300

    feat - added support for rendering svg components in native platforms

--- src/components/ImageSVG/index.native.tsx
+++ src/components/ImageSVG/index.native.tsx
@@ -1,10 +1,51 @@
 import {Image} from 'expo-image';
 import React from 'react';
 import type {ImageSourcePropType} from 'react-native';
+import type {SvgProps} from 'react-native-svg';
 import type ImageSVGProps from './types';
 
-function ImageSVG({src, width = '100%', height = '100%', fill, contentFit = 'cover', style}: ImageSVGProps) {
+function ImageSVG({
+    src,
+    width = '100%',
+    height = '100%',
+    fill,
+    contentFit = 'cover',
+    hovered = false,
+    pressed = false,
+    style,
+    pointerEvents,
+    preserveAspectRatio
+}: ImageSVGProps) {
     const tintColorProp = fill ? {tintColor: fill} : {};
+
+    if (typeof src === 'function') {
+        const ImageSvgComponent = src as React.FC<SvgProps>;
+        const additionalProps: Pick<ImageSVGProps, 'fill' | 'pointerEvents' | 'preserveAspectRatio'> = {};
+
+        if (fill) {
+            additionalProps.fill = fill;
+        }
+    
+        if (pointerEvents) {
+            additionalProps.pointerEvents = pointerEvents;
+        }
+    
+        if (preserveAspectRatio) {
+            additionalProps.preserveAspectRatio = preserveAspectRatio;
+        }
+
+        return (
+            <ImageSvgComponent
+                width={width}
+                height={height}
+                fill={fill}
+                hovered={`${hovered}`}
+                pressed={`${pressed}`}
+                style={style}
+                {...additionalProps}
+            />
+        )
+    }
 
     return (
         <Image
